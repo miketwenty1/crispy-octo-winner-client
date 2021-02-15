8cf8e7c2f6b967f99504174d56489c77481d2ccc
Michael Tidwell <tidwell@zebedee.io>
2021-02-15 00:26:33 -0500

    remove .env

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,45 @@ const path = require('path');
 const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 
+// PROD
 module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  resolve: {
+    fallback: {
+      fs: false,
+    },
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/build/',
+    filename: 'project.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: { loader: 'babel-loader' },
+        exclude: /node_modules/,
+      },
+      {
+        test: [/\.vert$/, /\.frag$/],
+        use: 'raw-loader',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true),
+      SERVER_URL: 'https://friday-nightz.herokuapp.com',
+    }),
+  ],
+};
+
+// DEV
+module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   resolve: {
     fallback: {
